153b5bc8602839ec77feed33576a3027d7014c60
syed <ibrahimsyed624@gmail.com>
2020-01-05 01:02:50 +0530

    Learning python day1 variables, string and some inbuilt functions

---
+++ day1/age.py
@@ -0,0 +1,3 @@
+birth_year = input("Enter your birth year : ")
+age = 2020 - int(birth_year)
+print(age)
---
+++ day1/str.py
@@ -0,0 +1,13 @@
+name="Syed Ibrahim"
+print(name[1:])
+first_name="syed ibrahim"
+last_name='thamimul ansari'
+message = f'{first_name} last name is {last_name}'
+print(message)
+
+mail_template='''Hello Name,
+This is syed
+
+Your's faithfully,
+Syed'''
+print(mail_template)
