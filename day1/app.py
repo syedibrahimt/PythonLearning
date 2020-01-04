125430d21ea4e6c4eb3b81ba07e162410b0fd8fe
syed <ibrahimsyed624@gmail.com>
2020-01-05 01:16:37 +0530

    Learning python day1 variables, string and some inbuilt functions code refactoring

--- day1/app.py
+++ day1/app.py
@@ -1,7 +1,7 @@
 print("Hello World!!!")
 print("*" * 10)
-is_good=True
-if(is_good):
+is_good = True
+if (is_good):
     print("Good Boy")
 name = input("What is your name? ")
 print("Hello " + name)
--- day1/str.py
+++ day1/str.py
@@ -1,11 +1,11 @@
-name="Syed Ibrahim"
+name = "Syed Ibrahim"
 print(name[1:])
-first_name="syed ibrahim"
-last_name='thamimul ansari'
+first_name = "syed ibrahim"
+last_name = 'thamimul ansari'
 message = f'{first_name} last name is {last_name}'
 print(message)
 
-mail_template='''Hello Name,
+mail_template = '''Hello Name,
 This is syed
 
 Your's faithfully,
