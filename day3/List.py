fff6c967122afea34350b64370dd59a8eb7dfa88
syedibrahimt <ibrahimsyed624@gmail.com>
2020-02-04 20:43:17 +0530

    day 3 evening session List tutorials

---
+++ day3/List.py
@@ -0,0 +1,11 @@
+my_list = []
+print(my_list)
+my_list.append('a')
+my_list.append('b')
+my_list.append('c')
+my_list.append('d')
+print(my_list)
+my_list.reverse()
+print(my_list)
+my_list.sort()
+print(my_list)
