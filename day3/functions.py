661ada15889cddf9b9d298ad32e536376b29ac55
syedibrahimt <ibrahimsyed624@gmail.com>
2020-02-05 20:03:49 +0530

    day 4 evening session Functions in python

---
+++ day3/functions.py
@@ -0,0 +1,12 @@
+def print_hello():
+    print("this is the sample function")
+
+
+print_hello()
+
+
+def parametered_function(name='syed'):
+    print(f'Hello {name} welcome to the world of python')
+
+
+parametered_function()
